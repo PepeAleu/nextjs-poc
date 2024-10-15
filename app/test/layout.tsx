7f9e59f93685165da3a25b7693d61c52d6776c1e
Pepe Aleu <107838627+josealnz@users.noreply.github.com>
2024-10-15 14:04:56 +0200

    css modules

---
+++ app/test/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function TestLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  );
+  
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from "./test.module.css";
+
+export default function Test() {
+  return (
+    <div className="{styles.grid}">
+      
+    </div>
+  );
+}
