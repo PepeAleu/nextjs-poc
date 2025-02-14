944484ee3b8b5ed3de64298a50d32352ef9ca712
Pepe Aleu <107838627+josealnz@users.noreply.github.com>
2025-02-14 13:15:09 +0100

    Fix lint issues

--- app/_utils/intl.ts
+++ app/_utils/intl.ts
@@ -1,7 +1,9 @@
 import { siteMetadata } from "../_data/siteMetadata";
 
 export function pluralize(count: number, singular: string, plural: string) {
-	const intl = new Intl.PluralRules(siteMetadata.language, { type: 'cardinal' });
+	const intl = new Intl.PluralRules(siteMetadata.language, {
+		type: "cardinal",
+	});
 	const pluralForm = intl.select(count);
-	return pluralForm === 'one' ? singular : plural;
+	return pluralForm === "one" ? singular : plural;
 }
